c83592944dc1edfa1e2ee253c7c1963f51806c40
Loic166 <kasseloicmekontchou@gmail.com>
2025-03-27 05:18:10 +0100

    second commit

--- test.c
+++ test.c
@@ -1,5 +1,32 @@
 # include <stdio.h>
+# include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+}node;
+
+node *Creat_node(int x){
+    node * new_node =(node*)malloc(sizeof(node));
+    new_node ->data = x;
+    new_node ->next = NULL;
+    return new_node;
+}
+
+void stamp_list(node *testa){
+    if(testa == NULL){
+        printf("la lista è vueìota");
+        return;
+    }
+    printf("{");
+    while(testa != NULL){
+        printf("%d ",testa->data);
+        testa = testa->next;
+    }
+    printf("}");
+}
 
 int main(){
-    printf("hello world");
+   node *a = Creat_node(23);
+    stamp_list(a);
 }
